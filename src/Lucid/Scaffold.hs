4694d00ceaf90f63cc961963e080e9f0ec67b2c2
Athan Clark <athan.clark@gmail.com>
2014-12-09 15:24:39 -0700

    cosmetic

--- src/Lucid/Scaffold.hs
+++ src/Lucid/Scaffold.hs
@@ -15,12 +15,12 @@ import Data.Monoid
 
 data WebPage m = WebPage {           pageTitle :: T.Text -- ^ Page title
                          ,             favicon :: T.Text -- ^ Favicon url
-                         ,            metaVars :: (HtmlT m ())
+                         ,            metaVars :: (HtmlT m ()) -- ^ @\<meta\>@ tags
                          ,         initScripts :: (HtmlT m ()) -- ^ JavaScript to include at the top of the page
-                         , beforeStylesScripts :: (HtmlT m ()) -- ^ JavaScript to include before @/<style/>@ tags
+                         , beforeStylesScripts :: (HtmlT m ()) -- ^ JavaScript to include before @\<style\>@ tags
                          ,              styles :: (HtmlT m ()) -- ^ Styles
-                         ,  afterStylesScripts :: (HtmlT m ()) -- ^ JavaScript to include after @/<style/>@ tags - see <http://modernizr.com Modernizr>
-                         ,         bodyScripts :: (HtmlT m ()) -- ^ JavaScript to include at the base of @/<body/>@
+                         ,  afterStylesScripts :: (HtmlT m ()) -- ^ JavaScript to include after @\<style\>@ tags - ie: <http://modernizr.com Modernizr>
+                         ,         bodyScripts :: (HtmlT m ()) -- ^ JavaScript to include at the base of @\<body\>@
                          }
 
 
@@ -28,8 +28,10 @@ instance Monad m => Monoid (WebPage m) where
   mempty = WebPage "" "" mempty mempty mempty mempty mempty mempty
   mappend x _ = x -- Some people just want to watch the world burn.
 
-
-template :: Monad m => WebPage m -> HtmlT m () -> HtmlT m ()
+template :: Monad m =>
+            WebPage m -- ^ Page information
+         -> HtmlT m () -- ^ Content to insert in @\<body\>@
+         -> HtmlT m ()
 template page content = doctypehtml_ $ mconcat $
   [ head_ [] $ mconcat $
       [ initScripts page
